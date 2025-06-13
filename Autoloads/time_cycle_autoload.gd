a3fe2314bbadd0ec99b2f97f99b8cbd88e17f3a8
WorkingCode23 <nickdemidenko32@gmail.com>
2025-06-13 18:48:00 +0200

    Tages Zyklos fertig
    Je nachdem welche Zeit es ist, wird kälter und dunkler; und andersrum
    Bug in TileMap-Trees gefunden und gefixt

--- Autoloads/time_cycle_autoload.gd
+++ Autoloads/time_cycle_autoload.gd
@@ -1,4 +1,39 @@
 extends Node
 
+const DAY_PART := 2.0 / 5.0
+const EVENING_PART := 1.0 / 5.0
+const NIGHT_PART := 1.0 / 5.0
+
+var coldness_at_day := 1.5
+var coldness_at_evening := 3.0
+var coldness_at_night := 5.0
+var mood_color_day : Color = Color(0.78, 0.78, 0.78)
+var mood_color_evening : Color = Color(0.61, 0.404, 0.342)
+var mood_color_night : Color = Color(0.182, 0.193, 0.23)
+
 ## zieht den fuel von Bonfire pro sekunde ab
 var current_coldness := 2.0
+var current_mood_color := Color(255)
+var time_cycle_duration := 300.0
+
+func _ready() -> void:
+	await get_tree().process_frame
+	var day_duration := time_cycle_duration * DAY_PART
+	var evening_duration := time_cycle_duration * EVENING_PART
+	var night_duration := time_cycle_duration * NIGHT_PART
+	var tween := create_tween().set_trans(Tween.TRANS_QUART).set_loops(3)
+	
+	current_coldness = coldness_at_night
+	current_mood_color = mood_color_night
+	# Night --> Evening
+	tween.tween_property(self, "current_mood_color", mood_color_evening, night_duration).set_ease(Tween.EASE_IN)
+	tween.parallel().tween_property(self, "current_coldness", coldness_at_evening, night_duration).set_ease(Tween.EASE_IN)
+	# Evening --> Day
+	tween.tween_property(self, "current_mood_color", mood_color_day, evening_duration).set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_property(self, "current_coldness", coldness_at_day, evening_duration).set_ease(Tween.EASE_OUT)
+	# Day --> Evening
+	tween.tween_property(self, "current_mood_color", mood_color_evening, day_duration).set_ease(Tween.EASE_IN)
+	tween.parallel().tween_property(self, "current_coldness", coldness_at_evening, day_duration).set_ease(Tween.EASE_IN)
+	# Evening --> Night
+	tween.tween_property(self, "current_mood_color", mood_color_night, evening_duration).set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_property(self, "current_coldness", coldness_at_night, evening_duration).set_ease(Tween.EASE_OUT)
--- Environment/level.gd
+++ Environment/level.gd
@@ -1,2 +1,30 @@
 class_name Level
 extends Node2D
+
+@export_range(10.0, 600.0, 0.5) var time_cycle_duration := 180.0
+@export_group("Coldness", "coldness")
+@export_range(0.01, 5.0, 0.01, "or_greater") var coldness_at_day := 1.5
+@export_range(0.01, 5.0, 0.01, "or_greater") var coldness_at_evening := 3.0
+@export_range(0.01, 5.0, 0.01, "or_greater") var coldness_at_night := 5.0
+@export_group("Time Color Mood", "mood_color")
+@export var mood_color_day : Color = Color(0.78, 0.78, 0.78)
+@export var mood_color_evening : Color = Color(0.61, 0.404, 0.342)
+@export var mood_color_night : Color = Color(0.182, 0.193, 0.23)
+
+@onready var _canvas := $CanvasModulate
+
+func _ready() -> void:
+	_time_cycle_setup()
+
+func _process(_delta: float) -> void:
+	_canvas.color = TimeCycleAutoload.current_mood_color
+
+func _time_cycle_setup() -> void:
+	var cycle := TimeCycleAutoload
+	cycle.coldness_at_day = coldness_at_day
+	cycle.coldness_at_evening = coldness_at_evening
+	cycle.coldness_at_night = coldness_at_night
+	cycle.mood_color_day = mood_color_day
+	cycle.mood_color_evening = mood_color_evening
+	cycle.mood_color_night = mood_color_night
+	cycle.time_cycle_duration = time_cycle_duration
