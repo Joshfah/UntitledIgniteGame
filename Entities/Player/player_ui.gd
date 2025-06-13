a09ffda3f941a769d43fb27d92379c4181d3501c
WorkingCode23 <nickdemidenko32@gmail.com>
2025-06-13 17:09:50 +0200

    bug fix
    PlayerUi in Player hinzugefügt

--- Entities/Player/player_ui.gd
+++ Entities/Player/player_ui.gd
@@ -15,11 +15,11 @@ func _ready() -> void:
 	
 	heartsprites = hearts.get_children()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var hearts_remainingf : float = float(health) / 2
 	hearts_remainingf += 0.5
 	
-	var hearts_remaining : int = hearts_remainingf
+	var hearts_remaining : int = int(hearts_remainingf)
 	
 	for n in range(heartsprites.size()):
 		if n <= hearts_remaining - 1:
