8561115ed579af67b42acb28858266d7713b9237
WorkingCode23 <nickdemidenko32@gmail.com>
2025-06-14 23:59:33 +0200

    Enemy Patrol State

--- Entities/Enemy/enemy.gd
+++ Entities/Enemy/enemy.gd
@@ -3,12 +3,15 @@ extends CharacterBody2D
 
 enum State {
 	IDLE,
+	PATROL,
 	CHASE,
 	ATTACK
 }
 
 @export_range(10.0, 100.0, 0.1, "or_greater") var normal_speed := 50.0
 @export_range(0.01, 1.0, 0.01) var acceleration := 0.05
+@export_range(1.0, 50.0, 0.5) var max_patrol_delay := 15.0
+@export_range(10.0, 300.0, 0.1) var max_patrol_distance := 192.0
 @export_group("Attack", "attack")
 ## Der Delay bevor die Attacke losgeht
 @export_range(0.1, 1.0, 0.1, "or_greater") var attack_prepare_duration := 0.5
@@ -28,6 +31,7 @@ enum State {
 @onready var _path_timer := $PathTimer
 @onready var _prepare_timer := $PrepareTimer
 @onready var _cooldown_timer := $AttackCooldownTimer
+@onready var _patrol_timer := $PatrolTimer
 @onready var _animated_sprite := $AnimatedSprite2D
 
 var state : State = State.IDLE
@@ -46,10 +50,13 @@ func _ready() -> void:
 	_nav_agent.velocity_computed.connect(_on_navigation_agent_2d_velocity_computed)
 	_path_timer.timeout.connect(_recalc_path)
 	_cooldown_timer.timeout.connect(_recover)
+	_patrol_timer.timeout.connect(_start_patrol)
 	# Preset
 	speed = normal_speed
 	_prepare_timer.wait_time = attack_prepare_duration
 	_cooldown_timer.wait_time = attack_cooldown
+	randomize()
+	_patrol_timer.start(randf() * max_patrol_delay)
 	if not PlayerAutoload.player:
 		await get_tree().process_frame
 	target = PlayerAutoload.player
@@ -64,6 +71,8 @@ func _physics_process(delta: float) -> void:
 				state = State.CHASE
 				_play("run")
 				print("is chasing")
+		State.PATROL:
+			_patrol()
 		State.CHASE:
 			_chase()
 		State.ATTACK:
@@ -75,7 +84,6 @@ func _chase() -> void:
 	var desired_velocity := direction * speed
 	var steering_vector := desired_velocity - velocity
 	_nav_agent.set_velocity(steering_vector)
-	#print(steering_vector)
 
 func _attack(delta: float) -> void:
 	if not _prepare_timer.is_stopped() or not _cooldown_timer.is_stopped():
@@ -92,6 +100,24 @@ func _attack(delta: float) -> void:
 		_cooldown_timer.start()
 		#state = State.CHASE
 		_play("idle")
+
+func _patrol() -> void:
+	_chase()
+	if _nav_agent.is_target_reached():
+		_patrol_timer.start(randf() * max_patrol_delay)
+		_play("idle")
+		state = State.IDLE
+
+func _start_patrol() -> void:
+	if not state == State.IDLE:
+		return
+	randomize()
+	var random_direction_vector := Vector2(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0)).normalized()
+	var random_position := random_direction_vector * max_patrol_distance * randf() + global_position
+	_nav_agent.set_target_position(random_position)
+	
+	_play("run")
+	state = State.PATROL
 
 func _recover() -> void:
 	state = State.CHASE
@@ -110,6 +136,8 @@ func _die() -> void:
 	queue_free()
 
 func _recalc_path() -> void:
+	if not state == State.CHASE:
+		return
 	_nav_agent.set_target_position(target.global_position)
 
 func _on_attack_area_body_entered(body: Node2D) -> void:
